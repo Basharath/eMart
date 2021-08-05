140d46634c2735e0e402dd57870a56a9af10bebc
Basharath <basharath.in@gmail.com>
2021-08-05 21:46:10 +0530

    Adds all APIs involved for fetching data

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,34 @@ import axios from 'axios';
 
 const url = 'https://emartt.herokuapp.com/api';
 const productsUrl = `${url}/products`;
+const ordersUrl = `${url}/orders`;
+const cartUrl = `${url}/cart`;
+const categoriesUrl = `${url}/categories`;
+const usersUrl = `${url}/users`;
 
-export const fetchProducts = () => axios.get(productsUrl);
-export const creatPost = (newProduct) => axios.post(productsUrl, newProduct);
+export const getProducts = () => axios.get(productsUrl);
+export const addProduct = (newProduct) => axios.post(productsUrl, newProduct);
+export const updateProduct = (newProduct) => axios.put(productsUrl, newProduct);
+export const deleteProduct = (id) => axios.delete(`${productsUrl}/${id}`);
+export const rateProduct = (id, rating) =>
+  axios.patch(`${productsUrl}/${id}`, rating);
+
+export const getOrders = () => axios.get(ordersUrl);
+export const postOrder = (newOrder) => axios.post(ordersUrl, newOrder);
+export const cancelOrder = (id) => axios.delete(`${ordersUrl}?id=${id}`);
+
+export const getCart = () => axios.get(cartUrl);
+export const updateCart = (newItem) => axios.post(cartUrl, newItem);
+export const clearCart = () => axios.delete(cartUrl);
+
+export const getCategories = () => axios.get(categoriesUrl);
+export const addCategory = (newCategory) =>
+  axios.post(categoriesUrl, newCategory);
+export const updateCategory = (id, updatedCategory) =>
+  axios.put(`${categoriesUrl}/${id}`, updatedCategory);
+export const deleteCategory = (id) => axios.put(`${categoriesUrl}/${id}`);
+
+export const signInUser = (userData) =>
+  axios.post(`${usersUrl}/signin`, userData);
+export const signUpUser = (userData) =>
+  axios.post(`${usersUrl}/signup`, userData);
