2da49689cd4f629f33b532e124cb1b9bd4ae1df5
Basharath <basharath.in@gmail.com>
2021-08-06 22:24:58 +0530

    Adds action types

---
+++ src/actionTypes/index.js
@@ -0,0 +1,5 @@
+export const FETCH_ALL = 'FETCH_ALL';
+export const FETCH_ONE = 'FETCH_ONE';
+export const DELETE_ONE = 'DELETE_ONE';
+export const UPDATE_ONE = 'UPDATE_ONE';
+export const RATE_PRODUCT = 'RATE_PRODUCT';
