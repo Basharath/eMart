2eea60e2df6a1d6db55003166bb4dfbf0920b965
Basharath <basharath.in@gmail.com>
2021-08-08 15:47:44 +0530

    Updates actions types and other config

--- src/actionTypes/index.js
+++ src/actionTypes/index.js
@@ -3,3 +3,6 @@ export const GET_PRODUCT = 'GET_PRODUCT';
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const RATE_PRODUCT = 'RATE_PRODUCT';
+
+export const AUTH = 'AUTH';
+export const LOGOUT = 'LOGOUT';
