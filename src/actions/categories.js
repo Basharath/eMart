1a4afcc60f4a217d127f165d3686a8d2374650c2
Basharath <basharath.in@gmail.com>
2021-08-11 21:24:09 +0530

    Add category reducer and actions

---
+++ src/actions/categories.js
@@ -0,0 +1,43 @@
+import {
+  GET_CATEGORIES,
+  ADD_CATEGORY,
+  UPDATE_CATEGORY,
+  DELETE_CATEGORY,
+} from '../actionTypes';
+import * as api from '../api';
+
+export const getCategories = () => async (dispatch) => {
+  try {
+    const { data } = await api.getCategories();
+    dispatch({ type: GET_CATEGORIES, payload: data });
+  } catch (err) {
+    console.log('Categories ERR', err.response.data);
+  }
+};
+
+export const addCategory = (category) => async (dispatch) => {
+  try {
+    const { data } = await api.addCategory(category);
+    dispatch({ type: ADD_CATEGORY, payload: data });
+  } catch (err) {
+    console.log('Categories ERR', err.response.data);
+  }
+};
+
+export const updateCategory = (id, category) => async (dispatch) => {
+  try {
+    const { data } = await api.updateCategory(id, category);
+    dispatch({ type: UPDATE_CATEGORY, payload: data });
+  } catch (err) {
+    console.log('Categories ERR', err.response.data);
+  }
+};
+
+export const deleteCategory = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.deleteCategory(id);
+    dispatch({ type: DELETE_CATEGORY, payload: data });
+  } catch (err) {
+    console.log('Categories ERR', err.response.data);
+  }
+};
---
+++ src/reducers/categories.js
@@ -0,0 +1,31 @@
+import {
+  GET_CATEGORIES,
+  ADD_CATEGORY,
+  UPDATE_CATEGORY,
+  DELETE_CATEGORY,
+} from '../actionTypes';
+
+const categoriesReducer = (categories = [], action) => {
+  switch (action.type) {
+    case GET_CATEGORIES:
+      return [...categories, ...action.payload];
+
+    case ADD_CATEGORY:
+      return [...categories, ...action.payload];
+
+    case UPDATE_CATEGORY:
+      return [
+        ...categories.map((c) =>
+          c._id === action.payload._id ? action.payload : c
+        ),
+      ];
+
+    case DELETE_CATEGORY:
+      return [...categories.filter((c) => c._id !== action.payload._id)];
+
+    default:
+      return categories;
+  }
+};
+
+export default categoriesReducer;
