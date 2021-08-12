ac9283310281a112b0b3e18af20104871ca404d5
Basharath <basharath.in@gmail.com>
2021-08-12 21:37:02 +0530

    Adds reducers to the combine list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,5 +3,7 @@ import { combineReducers } from 'redux';
 import products from './products';
 import cart from './cart';
 import auth from './auth';
+import orders from './orders';
+import categories from './categories';
 
-export default combineReducers({ products, auth, cart });
+export default combineReducers({ products, auth, cart, orders, categories });
