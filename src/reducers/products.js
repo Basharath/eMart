dc9006644d491b8257697896768418baee134add
Basharath <basharath.in@gmail.com>
2021-08-13 22:21:16 +0530

    Adds 'add product' case

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,6 +1,7 @@
 import {
   GET_PRODUCTS,
   GET_PRODUCT,
+  ADD_PRODUCT,
   UPDATE_PRODUCT,
   RATE_PRODUCT,
   DELETE_PRODUCT,
@@ -10,8 +11,13 @@ const productReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
       return { ...state, products: action.payload };
+
     case GET_PRODUCT:
       return { ...state, product: action.payload };
+
+    case ADD_PRODUCT:
+      return { ...state, products: [...state.products, action.payload] };
+
     case UPDATE_PRODUCT:
       return {
         ...state,
@@ -19,6 +25,7 @@ const productReducer = (state = { products: [] }, action) => {
           p._id === action.payload._id ? action.payload : p
         ),
       };
+
     case RATE_PRODUCT:
       return {
         ...state,
@@ -26,11 +33,13 @@ const productReducer = (state = { products: [] }, action) => {
           p._id === action.payload._id ? action.payload : p
         ),
       };
+
     case DELETE_PRODUCT:
       return {
         ...state,
         products: state.products.filter((p) => p._id !== action.payload),
       };
+
     default:
       return state;
   }
