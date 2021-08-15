305fe2865ab23e806997c2af7c91a2a957ecaf4b
Basharath <basharath.in@gmail.com>
2021-08-15 21:32:31 +0530

    Adds popup component for confirmations

---
+++ src/common/Popup.jsx
@@ -0,0 +1,24 @@
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+export default function Popup({ show, handleClose, handleConfirm }) {
+  return (
+    <Modal show={show} onHide={handleClose} size="lg" className="popup">
+      <Modal.Body className="d-flex flex-column align-items-center text-center p-4">
+        <p className="fs-5">Do you really want to delete the product?</p>
+        <div className="mt-2">
+          <Button variant="success" onClick={handleClose} className="mx-3 px-4">
+            No
+          </Button>
+          <Button
+            variant="danger"
+            onClick={handleConfirm}
+            className="mx-3 px-4"
+          >
+            Yes
+          </Button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
