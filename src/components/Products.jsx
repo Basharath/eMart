a88eaa3c47e0cc2dd4b3f5087c34613ab5f4d285
Basharath <basharath.in@gmail.com>
2021-08-18 21:07:34 +0530

    Renders product cards

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,21 @@
 import { useSelector } from 'react-redux';
+import ProductCard from '../common/ProductCard';
 
 export default function Products() {
   const { products } = useSelector((state) => state.products);
 
   return (
-    <div>
+    <div className="d-flex">
       {products?.map((p) => (
-        <div key={p._id}>{p.name}</div>
+        <ProductCard
+          name={p.name}
+          offer={p.offer}
+          price={p.price}
+          img={p.images[0]?.url}
+          rating={2.5}
+          classes="m-2"
+          key={p._id}
+        />
       ))}
     </div>
   );
