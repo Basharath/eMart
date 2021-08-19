409d5bbdff5cf0ee128fae2adc21fff25f85296a
Basharath <basharath.in@gmail.com>
2021-08-19 21:05:34 +0530

    Updates flex display of products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,14 +5,13 @@ export default function Products() {
   const { products } = useSelector((state) => state.products);
 
   return (
-    <div className="d-flex">
+    <div className="d-flex flex-wrap justify-content-center justify-content-lg-start">
       {products?.map((p) => (
         <ProductCard
           name={p.name}
           offer={p.offer}
           price={p.price}
           img={p.images[0]?.url}
-          rating={2.5}
           classes="m-2"
           key={p._id}
         />
