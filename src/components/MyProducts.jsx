355bf47e5b1e553e30e8f6facf5dfc34f4b8624b
Basharath <basharath.in@gmail.com>
2021-08-20 09:12:01 +0530

    Adds edit and delete btns to the product card

--- src/components/MyProducts.jsx
+++ src/components/MyProducts.jsx
@@ -1,40 +1,63 @@
-import { useEffect } from 'react';
-import Container from 'react-bootstrap/Container';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import Button from 'react-bootstrap/Button';
-// import { Link } from 'react-router-dom';
-import { NavLink, Link } from 'react-router-dom';
-import { getVendorProducts } from '../actions/products';
+import { Link } from 'react-router-dom';
+import Popup from '../common/Popup';
+import ProductCard from '../common/ProductCard';
+import { getVendorProducts, deleteProduct } from '../actions/products';
 import { getUser } from '../actions/auth';
 
 export default function MyProducts() {
   const { vendorProds: products = [] } = useSelector((state) => state.products);
+  const [show, setShow] = useState(false);
+  const [productId, setProductId] = useState(1);
   const dispatch = useDispatch();
-  const { isAdmin, id } = getUser();
+  const { isAdmin, id } = getUser() || {};
 
   useEffect(() => {
     dispatch(getVendorProducts(isAdmin ? '' : id));
   }, []);
 
+  const handleSetData = (status, prodId) => {
+    setShow(status);
+    setProductId(prodId);
+  };
+
+  const handleClose = () => setShow(false);
+
+  const handleDelete = () => {
+    handleClose();
+    dispatch(deleteProduct(productId));
+  };
+
   return (
-    <Container className="py-3">
+    <div className="py-3">
+      <Popup
+        show={show}
+        handleClose={handleClose}
+        handleConfirm={handleDelete}
+      />
       <Link to="/add-product" className="btn btn-warning d-block mx-auto w-25">
         Add product
       </Link>
-      <div>
-        <h3>Products</h3>
+      <h3 className="text-center my-3">My Products</h3>
+      <div className="d-flex flex-wrap justify-content-center justify-content-lg-start">
         {products?.map((p) => (
-          // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-          // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-          <NavLink
-            to={`update-product/${p._id}`}
+          <ProductCard
+            id={p._id}
+            name={p.name}
+            offer={p.offer}
+            price={p.price}
+            img={p.images[0]?.url}
             key={p._id}
-            className="d-block w-25 bg-warning my-1 text-dark"
-          >
-            {p.name}
-          </NavLink>
+            height="360px"
+            overflow="hidden"
+            classes="m-1"
+            myproduct
+            onEdit={`/update-product/${p._id}`}
+            onDelete={handleSetData}
+          />
         ))}
       </div>
-    </Container>
+    </div>
   );
 }
