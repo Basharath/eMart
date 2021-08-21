16b9be985849b166784861ee83737d6bdbc54c17
Basharath <basharath.in@gmail.com>
2021-08-21 22:36:13 +0530

    Adds a util func to capitalize the first letter

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
-export const getRandomRating = (m = 2, n = 5) => (Math.random() * (n - m)) + m; 
+export const getRandomRating = (m = 2, n = 5) => Math.random() * (n - m) + m;
 
-export const getRandomCount = (m = 1, n = 50) => Math.floor((Math.random() * (n - m + 1)) + m); 
+export const getRandomCount = (m = 1, n = 50) =>
+  Math.floor(Math.random() * (n - m + 1) + m);
+
+export const capitalizeFirst = (s) => s[0].toUpperCase() + s.slice(1);
