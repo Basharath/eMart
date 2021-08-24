f6f525f3fdf38f28fb934eac6243fac74204f167
Basharath <basharath.in@gmail.com>
2021-08-24 19:44:02 +0530

    Adds util to convert amount to locale string

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,8 @@ export const getRandomCount = (m = 1, n = 50) =>
   Math.floor(Math.random() * (n - m + 1) + m);
 
 export const capitalizeFirst = (s) => s[0].toUpperCase() + s.slice(1);
+
+export const convertAmount = (amount) =>
+  amount
+    .toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+    .slice(0, -3);
