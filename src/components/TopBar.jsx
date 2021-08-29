74fa3a48cd6d6383c3fdc75a1c7416d007167e34
Basharath <basharath.in@gmail.com>
2021-08-29 15:57:30 +0530

    Updates search submission

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,5 +1,6 @@
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
@@ -12,14 +13,32 @@ import logo from '../images/Logo.png';
 import NavItem from '../common/NavItem';
 import { logout } from '../actions/auth';
 import { CLEAR_CART, RESET_ORDERS } from '../actionTypes/index';
+import { getSearchedProducts } from '../actions/products';
 
 export default function TopBar({ user, cart }) {
+  const [search, setSearch] = useState('');
   const dispatch = useDispatch();
+  const history = useHistory();
+
+  const query = new URLSearchParams(useLocation().search).get('q');
+  useEffect(() => {
+    if (!query) setSearch('');
+  }, [query]);
 
   const handleLogout = () => {
     dispatch(logout());
     dispatch({ type: CLEAR_CART });
     dispatch({ type: RESET_ORDERS });
+  };
+
+  const handleChange = ({ currentTarget }) => setSearch(currentTarget.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (search) {
+      dispatch(getSearchedProducts(search));
+      history.push(`/search?q=${search}`);
+    } else history.push('/');
   };
 
   return (
@@ -31,13 +50,15 @@ export default function TopBar({ user, cart }) {
         </Link>
         <Navbar.Toggle aria-controls="navbarScroll" className="no-focus" />
         <Navbar.Collapse id="navbarScroll" className="">
-          <Form className="mx-auto w-50 search-mobile">
+          <Form className="mx-auto w-50 search-mobile" onSubmit={handleSubmit}>
             <InputGroup>
               <FormControl
                 placeholder="Search"
                 className="no-border"
                 type="search"
                 aria-label="Search"
+                value={search}
+                onChange={handleChange}
               />
               <InputGroup.Text className="bg-white no-border">
                 <i className="fas fa-search" />
